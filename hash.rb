5aa0f7574b3353293182da71db74263add521f18
ben <shawn704@yahoo.com>
2015-10-31 11:45:15 -0400

    hash.rb

---
+++ hash.rb
@@ -0,0 +1,18 @@
+student_age = {
+       'Jack'  => 10,
+       'Jill'  => 12,
+       'Bob'   => 14
+}
+resturant_menu = {
+         "Ramen" =>3 ,
+         "Dal Makhani" =>4 ,
+          "Tea" => 2
+          }
+# this will fetch value from hash when ran in terminal
+# puts resturant_menu["Ramen"]
+
+
+# this is used to modify a hash
+puts resturant_menu = {}
+puts resturant_menu["Dal Makhani"] = 4.5
+puts resturant_menu["Tea"] = 2
